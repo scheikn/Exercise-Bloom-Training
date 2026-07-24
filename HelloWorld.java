4c38e7739ed3df2f5d255162f265392ac6182c5d
scheikn <natascha.scheikl@takeda.com>
2026-07-24 10:30:28 +0000

    first example

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        int age = 1;
+        int temperature = -4;
+        char Kürzel = 'N';
+        System.out.println(age + temperature + Kürzel); 
+        System.out.println(age * temperature);
+    }
+}
