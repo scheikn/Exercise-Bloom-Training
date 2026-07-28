6b9d65c7a7fb717e88b156d17189c35af48b092f
scheikn <natascha.scheikl@takeda.com>
2026-07-28 09:19:35 +0000

    if, else, else if

---
+++ src/IFstatement.java
@@ -0,0 +1,19 @@
+public class IFstatement {
+    public static void main (String[] args) {
+        double age = 20.0;
+
+        if(age > 18) {
+            System.out.println("matches" + " " + age);
+        }
+        else if(age == 10) {
+            System.out.println ("invalid");
+        }
+        else {
+            System.out.println ("doesnt match");
+        }
+        
+
+        
+    }
+}
+//Comments, sieht er nicht
