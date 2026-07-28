ad38d0a48dcd4e5401978443719d279607986f06
scheikn <natascha.scheikl@takeda.com>
2026-07-28 07:33:54 +0000

    <second exercise students information, primitive data types>

---
+++ SecondExercise.java
@@ -0,0 +1,16 @@
+public class SecondExercise {
+
+    public static void main(String[] args) {
+        int studentAge = 16;
+        double studentGPA = 1.45;
+        char fristInitial = 'A';
+        char lastInitial = 'Z';
+        boolean perfectAttendence = true;
+
+        System.out.println(studentAge) ;
+        System.out.println(studentGPA) ;
+        System.out.println(fristInitial) ;
+        System.out.println(lastInitial) ;
+        System.out.println(perfectAttendence) ;
+    }
+}
