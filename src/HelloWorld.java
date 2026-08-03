a287403fb3ec27ff9080637ffa177734948a0fa3
scheikn <natascha.scheikl@takeda.com>
2026-08-03 17:30:29 +0000

    erste übung pos/neg/null

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
         int age = 1;
         int temperature = -4;
         char Kürzel = 'N';
-        System.out.println(age + temperature + Kürzel); 
+        System.out.println(age + temperature); 
+        System.out.println(Kürzel); 
         System.out.println(age * temperature);
     }
 }                                                                                                               
---
+++ src/HomeWork.java
@@ -0,0 +1,15 @@
+public class HomeWork {
+    public static void main (String[] args) {
+        int number = -2;
+
+        if (number > 0) {
+            System.out.println("positive");
+         }
+        else if (number < 0) {
+            System.out.println("negative");
+        }
+        else if (number == 0) {
+            System.out.println("Null");
+        }
+    }
+}
