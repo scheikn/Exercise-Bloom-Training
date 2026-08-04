2babfae5eaadd0a484757a448a7018a8d2fbd731
scheikn <natascha.scheikl@takeda.com>
2026-08-04 08:55:07 +0000

    übung strom

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,17 +1,18 @@
 public class HomeWork { 
     public static void main (String[] args) {
-        String username = "admin";
-        String password = "java123";
-
-    if (username.equals("admin")) {
-        if(password.equals("java123"))
-        System.out.println("login successfull");
-    }
-    else {
-     System.out.println("invalid login");
-    }
-        
+        int strom= 100;
+             
+        if (strom >= 300) {
+            System.out.println("Hoher Verbrauch");
         }
-    }   
-
- 
+        else if (strom >= 100 && strom <= 299) {
+            System.out.println("Normaler Verbrauch");
+        }
+        else if (strom <= 99) {
+            System.out.println("Niedriger Verbrauch");
+         }    
+        else {
+            System.out.println("invalid");
+        }
+    }
+    }
