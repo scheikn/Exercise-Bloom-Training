fb42bece96398eb1c05c54aea16284dfff939ebd
scheikn <natascha.scheikl@takeda.com>
2026-08-04 06:13:25 +0000

    Alterstabelle mit invalid als nesetd if

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,20 +1,27 @@
 public class HomeWork { 
     public static void main (String[] args) {
-        int number = -10;
+        int number = -3;
 
-        if (number >= 30) {
-            System.out.println("Heiß");  
+        if (number >= 65) {
+            System.out.println("Senior");  
         }
-        else if (number >= 20) {
-            System.out.println("Angenehm"); 
+        else if (number >= 18) {
+            System.out.println("Erwachsener"); 
         }
-        else if (number >= 0) {
-            System.out.println("Kalt");
+        else if (number >= 13) {
+            System.out.println("Jugendlicher");
         }
+
+        else { 
+
         if (number < 0) {
-            System.out.println("Gefrierpunkt"); 
-         }
-    }
-        
+            System.out.println("invalid"); 
+        }
+            else  {
+            System.out.println("Kind");
+            }
+        }
+   }      
+
  }  
 
