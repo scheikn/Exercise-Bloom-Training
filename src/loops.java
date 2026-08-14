543ad573eede9729543ae8690503e7e8cecfba72
scheikn <natascha.scheikl@takeda.com>
2026-08-14 05:38:19 +0000

    for loop zahlen 1 bis 10

---
+++ src/loops.java
@@ -0,0 +1,14 @@
+public class loops {
+    public static void main (String[] args) {
+        int i = 0;
+
+        while(i < 3) {
+            System.out.println(i);
+             i += 10;
+        }
+        for(int j = 5; j > 3; j--) {
+            System.out.println(j);
+        }
+        }
+
+        }
