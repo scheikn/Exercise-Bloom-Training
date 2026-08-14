344b130e8983662a601804b674b68da48744c1a1
scheikn <natascha.scheikl@takeda.com>
2026-08-14 07:31:03 +0000

    7 multipliziert mit 1 bis 10 tabelle

--- src/HomeWorkLoop.java
+++ src/HomeWorkLoop.java
@@ -1,11 +1,10 @@
 public class HomeWorkLoop { 
-    public static void main (String[] args) {
-     
-    for (int i =1; i<=30; i++) {
-        if (i %2 ==0)
-        System.out.println(i + " " + "gerade");  
-        if (i %2 !=0)
-         System.out.println(i + " " + "ungerade");  
+    public static void main(String[] args) {
+
+        int number = 7;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " " +  "x" + " " + i  + " " +"=" + number * i);
         }
     }
 }
