b30bc04e646a975353e90a8fe2e529fa1e9ac59a
scheikn <natascha.scheikl@takeda.com>
2026-08-14 06:14:17 +0000

    alle geraden zahlen von 2 bis 20 mit % in for loop mit if

--- src/HomeWorkLoop.java
+++ src/HomeWorkLoop.java
@@ -1,8 +1,10 @@
 public class HomeWorkLoop { 
     public static void main (String[] args) {
      
-       for (int i= 1; i <= 10; i++) {
-        System.out.println(i);
-}
-}
+
+        for (int i =2 ; i<=20; i++ ) 
+             if (i % 2 == 0) {
+            System.out.println(i);
+        }
+    }
 }
