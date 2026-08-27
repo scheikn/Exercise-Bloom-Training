311a104c41c91890cceb49735e1eccd77d938a4c
scheikn <natascha.scheikl@takeda.com>
2026-08-27 07:18:30 +0000

    drei products hintereinander und dann in mein berechnet

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main { 
     public static void main (String[] args) {
 
-        Product productA = new Product ("pen",3.5,6);     
+        Product productA = new Product ("pen",3.25,6);     
         productA.introduce (); 
   
+        Product productB = new Product ("calculator",10.99,2);     
+        productB.introduce (); 
+
+        Product productC = new Product ("paper",0.50,50);     
+        productC.introduce (); 
+
+        double totalPrice = productA.price*productA.quantity + productB.price*productB.quantity  + productC.price*productC.quantity ;
+
+        System.out.println("The total price is : " + totalPrice);
        }
 }
