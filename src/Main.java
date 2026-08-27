8641d2166425771fc250379efa12ffb80585842f
scheikn <natascha.scheikl@takeda.com>
2026-08-27 06:07:48 +0000

    4 students with grades

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main { 
     public static void main (String[] args) {
 
-        StudentGrades studentA = new StudentGrades ("Tom", 75);
+        StudentGrades studentA = new StudentGrades ("Tom", 10);
         studentA.introduce (); 
     
+        StudentGrades studentB = new StudentGrades ("Lisa", 63);
+        studentB.introduce (); 
+
+        StudentGrades studentC = new StudentGrades ("Tim", 99);
+        studentC.introduce (); 
+
+        StudentGrades studentD = new StudentGrades ("Anna", 35);
+        studentD.introduce (); 
+
        }
 }
